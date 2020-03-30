6afd93305f6854932d3a9dadda965efbb9009135
Robert Harrison <pollariss85@gmail.com>
2020-03-30 13:44:47 -0400

    Roll The Dice App

--- Roll The Dice/ViewController.swift
+++ Roll The Dice/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import AudioToolbox
+
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var rollOutlet: UIButton!
+   
+    
+let diceArray = [ #imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2 1x"), #imageLiteral(resourceName: "dice3 1x"), #imageLiteral(resourceName: "dice 4 1x"), #imageLiteral(resourceName: "dice5 1x"), #imageLiteral(resourceName: "dice 6 1x") ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rollOutlet.layer.cornerRadius = 15
+        
+
+        }
+
+     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEvent.EventSubtype.motionShake{
+            print("Shake")
+               diceImageView1.image = diceArray.randomElement()
+            diceImageView2.image = diceArray.randomElement()
+            AudioServicesPlaySystemSound(1520)
+        }
+        else {
+            print("nil")
+        }
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+      
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+        AudioServicesPlaySystemSound(1520)
+        
+    }
+    
+    
 }
 
